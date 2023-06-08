d3753537e6dc5bc1c6117f941440ee407b7259bf
j4ckd4n <j4ckd4n@doesnotexist.com>
2023-06-08 16:54:21 -0500

    Modified build settings, currently broken

--- build.ps1
+++ build.ps1
@@ -8,7 +8,8 @@ Write-Output "[BUILD]: Running 'pyinstaller' against SocPuppet.spec"
 .\\.venv\\Scripts\\pyinstaller.exe .\SocPuppet.spec --noconfirm
 
 Write-Output "[BUILD]: Copying over required files to the dist folder"
-Copy-Item -Recurse .\.venv\Lib\site-packages\gpt4all\* .\dist\SocPuppet -Verbose
+Copy-Item -Recurse .\.venv\Lib\site-packages\gpt4all\* .\dist\SocPuppet\gpt4all\ -Verbose
+Copy-Item -Recurse .\.venv\Lib\site-packages\grapheme\* .\dist\SocPuppet\grapheme\ -Verbose
 Copy-Item .\config.json .\dist\SocPuppet -Verbose
 
 Write-Output "[BUILD]: Creating a zip SocPuppet-$version.zip"
@@ -17,3 +18,4 @@ if (Test-Path -Path ".\dist\SocPuppet-$version.zip") {
 }
 
 Compress-Archive .\dist\SocPuppet ".\dist\SocPuppet-$version.zip"
+Remove-Item -Recurse -Force .\dist\SocPuppet
