805207428927cd83ae5289825ae6cb1e93e6a6a6
j4ckd4n <j4ckd4n@doesnotexist.com>
2023-06-15 16:30:55 -0500

    Supposedly fixed the ShodanLookup issue

--- Plugins/API/ShodanLookup.py
+++ Plugins/API/ShodanLookup.py
@@ -1,6 +1,6 @@
 from Plugins import Plugin
-from shodan import Shodan
 
+import requests
 import os
 
 class ShodanLookup(Plugin.Plugin):
@@ -8,40 +8,43 @@ class ShodanLookup(Plugin.Plugin):
     super().__init__(name)
     self._value = value
     self._api_key = os.getenv("SHODAN_API_KEY")
+    self._shodan_api_host = "https://api.shodan.io/shodan/host"
 
-  # TODO: This is broken, seems like the exception being thrown is "banner" and nothing else. May need to convert to 
   def _performLookup(self, value) -> dict:
     if self._api_key == None:
       return {
-        value:{
+        value: {
           'err': "No SHODAN_API_KEY environment value set."
         }
       }
-
-    try:
-      shodan = Shodan(self._api_key)
-      host = shodan.host(value)
-      data_out = {
-        value: {
-          "ip": value,
-          "organization": host.get("org", "n/a"),
-          "operating_system": host.get("os", "n/a"),
-          "additional_data": {}
-        }
-      }
-
-      for item in host['data']:
-        data_out[value]['additional_data'] = {
-          "port": item['port'],
-          "banner": item['banner']
-        }
-      return data_out
-    except Exception as e:
+    
+    res = requests.get(f"{self._shodan_api_host}/{value}?key={self._api_key}")
+    if res.status_code != 200:
       return {
         value: {
-          'err': e
+          'err': res.content
         }
       }
+    
+    host = res.json()
+    data_out = {
+      value: {
+        "organization": host['org'] if "org" in host.keys() else "n/a",
+        "isp": host['isp'] if 'isp' in host.keys() else "n/a",
+        "asn": host['asn'] if 'asn' in host.keys() else "n/a",
+        "operating_system": host['data']['os'] if "os" in host['data'] else "n/a",
+        "additional_data": []
+      }
+    }
+    
+    for data in host['data']: 
+      data_out[value]['additional_data'].append({
+        "port": data['port'] if 'port' in data else "n/a",
+        "banner": data['data'][:64] if 'data' in data else "n/a"
+      })
+
+    return data_out
+
 
   def run(self):
     print("\n ------------------------------- ")
@@ -62,10 +65,10 @@ class ShodanLookup(Plugin.Plugin):
     print("""
 IP:               %s
 Organization:     %s
-Operating System: %s""" % (host['ip'], host['organization'], host['operating_system']))
+Operating System: %s""" % (self._value, host['organization'], host['operating_system']))
 
     print("Banner Info:")
     for item in host['additional_data']:
       print(f"  Port: {item['port']}")
-      print(f"    Banner: \n{item['data']}")
+      print(f"    Banner: \n{item['banner']}")
       print() # ❤ i think we need some space...
